4eaa3682efba3cf3e2d3567709614d23fdc7e6da
G5277 <139153421+G5277@users.noreply.github.com>
2025-01-04 22:29:34 +0530

    visualizations added

--- main.py
+++ main.py
@@ -42,58 +42,58 @@ print('------------------------------------------------')
 print('Classification Report:')
 print(classification_report(y_test, predicted))
 
-# import numpy as np
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# from wordcloud import WordCloud
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
-# # Confusion Matrix Visualization
-# cf_matrix = confusion_matrix(y_test, predicted)
+# Confusion Matrix Visualization
+cf_matrix = confusion_matrix(y_test, predicted)
 
-# group_names = ['TP','TN','FP','FN']
-# group_counts = ['{0:0.0f}'.format(value) for value in cf_matrix.flatten()]
-# group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten()/np.sum(cf_matrix)]
+group_names = ['TP','TN','FP','FN']
+group_counts = ['{0:0.0f}'.format(value) for value in cf_matrix.flatten()]
+group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten()/np.sum(cf_matrix)]
 
-# labels = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in zip(group_names,group_counts, group_percentages)]
-# labels = np.asarray(labels).reshape(2,2)
+labels = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in zip(group_names,group_counts, group_percentages)]
+labels = np.asarray(labels).reshape(2,2)
 
-# sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Greens')
+sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Greens')
 
-# plt.title('Confusion Matrix')
-# plt.show()
+plt.title('Confusion Matrix')
+plt.show()
 
-# # Accuracy Visualization
-# accuracy_score = metrics.accuracy_score(predicted, y_test)
-# plt.figure(figsize=(10, 2))
-# plt.bar(['Accuracy'], [accuracy_score], color=['green'])
-# plt.ylim(0, 1)
-# plt.title('Model Accuracy')
-# plt.show()
-# print('ComplementNB model accuracy is', str('{:04.2f}'.format(accuracy_score*100))+'%')
+# Accuracy Visualization
+accuracy_score = metrics.accuracy_score(predicted, y_test)
+plt.figure(figsize=(10, 2))
+plt.bar(['Accuracy'], [accuracy_score], color=['green'])
+plt.ylim(0, 1)
+plt.title('Model Accuracy')
+plt.show()
+print('ComplementNB model accuracy is', str('{:04.2f}'.format(accuracy_score*100))+'%')
 
-# # Word Cloud Visualization
-# def plot_wordcloud(text, title):
-#     wordcloud = WordCloud(width=800, height=400,background_color='white').generate(text)
-#     plt.figure(figsize=(10, 5))
-#     plt.imshow(wordcloud, interpolation='bilinear')
-#     plt.axis('off')
-#     plt.title(title)
-#     plt.show()
+# Word Cloud Visualization
+def plot_wordcloud(text, title):
+    wordcloud = WordCloud(width=800, height=400,background_color='white').generate(text)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title(title)
+    plt.show()
 
-# # Sentiment Positive
-# positive_text = ' '.join(data[data['sentiment'] == 1]['text'])
-# plot_wordcloud(positive_text, 'Word Cloud for Positive Sentiment')
+# Sentiment Positive
+positive_text = ' '.join(data[data['sentiment'] == 1]['text'])
+plot_wordcloud(positive_text, 'Word Cloud for Positive Sentiment')
 
-# # Sentiment Positive
-# negatif_text = ' '.join(data[data['sentiment'] == 0]['text'])
-# plot_wordcloud(negatif_text, 'Word Cloud for Negative Sentiment')
+# Sentiment Positive
+negatif_text = ' '.join(data[data['sentiment'] == 0]['text'])
+plot_wordcloud(negatif_text, 'Word Cloud for Negative Sentiment')
 
-# # Visualization Sentiment
-# data['sentiment'] = data['sentiment'].replace({1: 'Positif', 0: 'Negatif'})
-# sentiment_counts = data['sentiment'].value_counts()
+# Visualization Sentiment
+data['sentiment'] = data['sentiment'].replace({1: 'Positif', 0: 'Negatif'})
+sentiment_counts = data['sentiment'].value_counts()
 
-# # Visualization with pie charts
-# plt.figure(figsize=(6, 6))
-# plt.pie(sentiment_counts, labels=sentiment_counts.index,autopct='%1.1f%%', startangle=90, colors=['lightcoral', 'lightskyblue'])
-# plt.title('Distribution of Sentiments')
-# plt.show()
+# Visualization with pie charts
+plt.figure(figsize=(6, 6))
+plt.pie(sentiment_counts, labels=sentiment_counts.index,autopct='%1.1f%%', startangle=90, colors=['lightcoral', 'lightskyblue'])
+plt.title('Distribution of Sentiments')
+plt.show()
